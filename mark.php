ad095d2e981ddeae2f5db7020bb84ca120a2a218
Joe Ainsworth <joe.ainsworth@me.com>
2016-03-19 09:25:02 +0000

    PHP to do list

---
+++ mark.php
@@ -0,0 +1,39 @@
+<?php
+
+require 'app/init.php';
+
+if(isset($_GET['as'], $_GET['item'])) {
+    $as     = $_GET['as'];
+    $item   = $_GET['item'];
+    
+    switch($as) {
+        case 'done':
+            $doneQuery = $db->prepare("
+                UPDATE items
+                SET done = 1
+                WHERE id = :item
+                AND user = :user
+            ");
+            
+            $doneQuery->execute([
+               'item'   => $item,
+               'user'   => $_SESSION['user_id'] 
+            ]);
+            break;
+        case 'undone':
+            $undoneQuery = $db->prepare("
+                UPDATE items
+                SET done = 0
+                WHERE id = :item
+                AND user = :user
+            ");
+            
+            $undoneQuery->execute([
+               'item'   => $item,
+               'user'   => $_SESSION['user_id'] 
+            ]);            
+            break;
+    }
+}
+
+header('Location: index.php');
